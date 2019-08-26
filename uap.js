2e0c9bdfa836a5c7bb5dfce3974aec6626d0610c
Siddhesh Zantye <siddheshadsv@icloud.com>
2019-08-26 18:05:51 +1200

    Update uap.js

--- uap.js
+++ uap.js
@@ -7,11 +7,11 @@ if (form.username.value == username[0] && form.password.value == password[0] || 
 self.location.href = page;
 alert("Welcome Siddhesh")
 }
-if (form.username.value == username[1] && form.password.value == password[1] || form.username.value == username[1] && form.password.value == password[1]) {
+else if (form.username.value == username[1] && form.password.value == password[1] || form.username.value == username[1] && form.password.value == password[1]) {
 self.location.href = pagev;
 alert("Welcome Vedant")
 }
-if (form.username.value != username[0] && form.password.value != password[0] || form.username.value == username[1] && form.password.value == password[1]) {
+else if (form.username.value != username[0] && form.password.value != password[0] || form.username.value == username[1] && form.password.value == password[1]) {
 alert("Sorry, Your Username or Password may be entered incorrectly.\nPlease try again.");
 form.username.focus();
 }
