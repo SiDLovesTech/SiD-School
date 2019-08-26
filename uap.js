7f8991cb053e5448c5719e56dd300cd227933048
Siddhesh Zantye <siddheshadsv@icloud.com>
2019-08-26 17:57:31 +1200

    e

--- uap.js
+++ uap.js
@@ -1,11 +1,21 @@
 function Login(form) {
-username = new Array("SidO","vedant", "abhijitz", "deepti", "", "",);
-password = new Array("Owner","5529", "abhijit2018", "deepti2018", "", "",);
-page = "s.html";
-if (form.username.value == username[0] && form.password.value == password[0] || form.username.value == username[1] && form.password.value == password[1] || form.username.value == username[2] && form.password.value == password[2] || form.username.value == username[3] && form.password.value == password[3] || form.username.value == username[4] && form.password.value == password[4] || form.username.value == username[5] && form.password.value == password[5] || form.username.value == username[6] && form.password.value == password[6] || form.username.value == username[7] && form.password.value == password[7] || form.username.value == username[8] && form.password.value == password[8] || form.username.value == username[9] && form.password.value == password[9]) {
+username = new Array("Siddhesh", "Vedant");
+password = new Array("Siddhesh", "Vedant");
+page = "siddhesh.html";
+pagev = "vedant.html";
+if (form.username.value == username[0] && form.password.value == password[0] || form.username.value == username[1] && form.password.value == password[1]) {
 self.location.href = page;
+alert("Welcome Siddhesh")
 }
-else {
+if (form.username.value == username[1] && form.password.value == password[1] || form.username.value == username[1] && form.password.value == password[1]) {
+self.location.href = pagev;
+alert("Welcome Vedant")
+}
+if (form.username.value != username[0] && form.password.value != password[0] || form.username.value == username[1] && form.password.value == password[1]) {
+alert("Sorry, Your Username or Password may be entered incorrectly.\nPlease try again.");
+form.username.focus();
+}
+if (form.username.value != username[1] && form.password.value != password[1] || form.username.value == username[1] && form.password.value == password[1]) {
 alert("Sorry, Your Username or Password may be entered incorrectly.\nPlease try again.");
 form.username.focus();
 }
