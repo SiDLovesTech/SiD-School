02b373e14083c4fc425eecec05db03451775b8a7
Siddhesh Zantye <siddheshadsv@icloud.com>
2019-08-27 18:30:35 +1200

    e

--- uap.js
+++ uap.js
@@ -5,17 +5,13 @@ page = "siddhesh.html";
 pagev = "vedant.html";
 if (form.username.value == username[0] && form.password.value == password[0] || form.username.value == username[1] && form.password.value == password[1]) {
 self.location.href = page;
-alert("Welcome Siddhesh")
 }
 if (form.username.value == username[1] && form.password.value == password[1] || form.username.value == username[1] && form.password.value == password[1]) {
 self.location.href = pagev;
-alert("Welcome Vedant")
 }
 else if (form.username.value != username[0] && form.password.value != password[0] || form.username.value == username[1] && form.password.value == password[1]) {
-alert("Sorry, Your Username or Password may be entered incorrectly.\nPlease try again.");
 }
 else if (form.username.value != username[1] && form.password.value != password[1] || form.username.value == username[1] && form.password.value == password[1]) {
-alert("Sorry, Your Username or Password may be entered incorrectly.\nPlease try again.");
 }
 return true;
 }
